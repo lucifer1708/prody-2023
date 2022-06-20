bdeeae71ab436db272ca245b11e061ae0057584b
lucifer1708 <20bce091@nith.ac.in>
2022-06-20 20:05:29 +0530

    added static and media urls for debug =False

--- prody/urls.py
+++ prody/urls.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include, re_path
+from django.views.static import serve
 from events import views as user_views
 from allauth.account.views import confirm_email
 from django.conf.urls.static import static
@@ -13,4 +14,11 @@ urlpatterns = [
             name='account_confirm_email'),
     path('events/', include('events.urls')),
     path('sponsors/', user_views.SponsorsList.as_view(), name='sponsors'),
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
